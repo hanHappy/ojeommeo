145db4c3bc43ddef3065a525db2bffef83e30b2f
han <hansmin95@gmail.com>
2024-11-14 23:11:09 +0900

    initial commit

---
+++ ojeommeo-backend/src/main/kotlin/com/ojeommeo/OjeommeoApplication.kt
@@ -0,0 +1,11 @@
+package com.ojeommeo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OjeommeoApplication
+
+fun main(args: Array<String>) {
+	runApplication<OjeommeoApplication>(*args)
+}
---
+++ ojeommeo-backend/src/test/kotlin/com/ojeommeo/OjeommeoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ojeommeo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OjeommeoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
